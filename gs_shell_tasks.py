3268caaa64f690e9905d8294da182fbd726e4563
PQ Groundstation <cmurexlab@gmail.com>
2022-12-23 09:02:34 -1000

    accept header from send_command

--- gs_shell_tasks.py
+++ gs_shell_tasks.py
@@ -2,12 +2,13 @@ from gs_commands import *
 
 
 async def send_command_task(radio, command_bytes, args, will_respond, debug=False):
-    success, response = send_command(radio, command_bytes, args, will_respond, debug=debug)
+    success, header, response = await send_command(radio, command_bytes, args, will_respond, debug=debug)
     if success:
         print("Command successful")
         print(f"Response: {response}")
     else:
         print("Command failed")
+    return success, header, response
 
 async def read_loop(radio):
 
