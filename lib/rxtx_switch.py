305cafc0e632484547f9f0a13d9156a63db271f8
Jacob Willis <jbwillis272@gmail.com>
2023-01-10 17:14:36 -0500

    make outputs

--- lib/rxtx_switch.py
+++ lib/rxtx_switch.py
@@ -5,8 +5,12 @@ class RXTXSwitch:
     def __init__(self, switch_pin, rx_led_pin, tx_led_pin, tx_value=True):
 
         self.switch = digitalio.DigitalInOut(switch_pin)
+        self.switch.switch_to_output(value=(not tx_value))
         self.rx_led = digitalio.DigitalInOut(rx_led_pin)
+        self.rx_led.switch_to_output(value=False)
         self.tx_led = digitalio.DigitalInOut(tx_led_pin)
+        self.tx_led.switch_to_output(value=False)
+
         self.tx_value = tx_value
 
     def transmit(self):
