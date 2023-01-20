200a6004b417c82e203eb8ce2de49eb45da3ba0f
Jacob Willis <jbwillis272@gmail.com>
2023-01-20 13:59:51 -0500

    smaller ack delay to increase throughput

--- lib/configuration/radio_configuration.py
+++ lib/configuration/radio_configuration.py
@@ -9,8 +9,8 @@ FREQUENCY = 433.0  # MHz
 FREQUENCY_DEVIATION = 10000  # Hz
 RX_BANDWIDTH = 25.0  # KHz
 PREAMBLE_LENGTH = 16  # bytes
-ACK_DELAY = 1.0  # seconds
-ACK_WAIT = 5  # seconds
+ACK_DELAY = 0.1  # seconds
+ACK_WAIT = 1  # seconds
 RECEIVE_TIMEOUT = 0.5  # seconds
 
 SATELLITE_ID = 0xAB
